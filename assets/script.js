08fc29343b2066ccaceff27e169907fb0af7fec9
Rich Armstrong <rich@taptapkaboom.com>
2023-05-29 11:57:10 +0200

    Added some stuff to masontry layout

--- assets/script.js
+++ assets/script.js
@@ -277,6 +277,12 @@ $(function() {
   loadRandomItemsData(function () {
     replaceRandomItemsWithData()
   });
+  
+  $('.masonry').masonry({
+    itemSelector: '.item',
+    columnWidth: 332,
+    gutter: 32,
+  });
 });
 
 /*
@@ -288,48 +294,50 @@ $desktopMin: 1080px;
  */
 // glider
 window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
-   //  slidesToShow: 3,
-	  // slidesToScroll: 3,
-	  draggable: true,
-	  dots: '.dots',
-	  responsive: [
-    {
-      // screens greater than >= 775px
-      breakpoint: 300,
-      settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        //arrows: false,
-        //itemWidth: 150,
-        duration: 0.25
-      }
-    },
-    {
-      // screens greater than >= 775px
-      breakpoint: 500,
-      settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        //arrows: false,
-        //itemWidth: 150,
-        duration: 0.25
-      }
-    },
-    {
-      // screens greater than >= 1024px
-      breakpoint: 720,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3
-        // itemWidth: 150,
-        // duration: 0.25
-      }
-    }
-  ]
-  })
+  if (document.querySelector('.glider')) {
+  	  new Glider(document.querySelector('.glider'), {
+  	   //  slidesToShow: 3,
+  		  // slidesToScroll: 3,
+  		  draggable: true,
+  		  dots: '.dots',
+  		  responsive: [
+  	    {
+  	      // screens greater than >= 775px
+  	      breakpoint: 300,
+  	      settings: {
+  	        // Set to `auto` and provide item width to adjust to viewport
+  	        slidesToShow: 1,
+  	        slidesToScroll: 1,
+  	        //arrows: false,
+  	        //itemWidth: 150,
+  	        duration: 0.25
+  	      }
+  	    },
+  	    {
+  	      // screens greater than >= 775px
+  	      breakpoint: 500,
+  	      settings: {
+  	        // Set to `auto` and provide item width to adjust to viewport
+  	        slidesToShow: 2,
+  	        slidesToScroll: 2,
+  	        //arrows: false,
+  	        //itemWidth: 150,
+  	        duration: 0.25
+  	      }
+  	    },
+  	    {
+  	      // screens greater than >= 1024px
+  	      breakpoint: 720,
+  	      settings: {
+  	        slidesToShow: 3,
+  	        slidesToScroll: 3
+  	        // itemWidth: 150,
+  	        // duration: 0.25
+  	      }
+  	    }
+  	  ]
+  	  })
+  }
 })
 
 
---
+++ pages/portfolio.md
@@ -0,0 +1,25 @@
+---
+title: Portfolio
+subtitle: See my work
+permalink: /portfolio/
+image: 'img/rich-doodle-thumb.jpg'
+background_color: black
+color: white
+related:
+  - /classes
+  - /workouts
+  - /posts
+---
+
+<div class="masonry">
+  <div class="item"><img src="../img/26-freedom.png" alt=""></div>
+  <div class="item"><img src="../img/beta-betty-print-2.jpg" alt=""></div>
+  <div class="item"><img src="../img/ddv-banner2.jpeg" alt=""></div>
+  <div class="item"><img src="../img/finish-strong-website-thumb.jpg" alt=""></div>
+  <div class="item"><img src="../img/hs-banner.jpg" alt=""></div>
+  <div class="item"><img src="../img/ugly-sketchbooks-thumb.png" alt=""></div>
+  <div class="item"><img src="../img/the-perfect-100dp-book.jpg" alt=""></div>
+  <div class="item"><img src="../img/skwgglz-banner.png" alt=""></div>
+  <div class="item"><img src="../img/rwd-wireframes.jpg" alt=""></div>
+  <div class="item"><img src="../img/rich-face-illo-1.jpg" alt=""></div>
+</div>
